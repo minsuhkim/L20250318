446e38eb4521fc79c0ab13906b54a7cff5e844db
minsuhkim <124859537+minsuhkim@users.noreply.github.com>
2025-03-25 16:47:49 +0900

    Thread & MySQL

--- Client/Program.cs
+++ Client/Program.cs
@@ -7,45 +7,69 @@ using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 
 namespace Client
 {
+
     class Program
     {
-        static void Main(string[] args)
+        static void SendPacket(Socket toSocket, string message)
         {
-            string jsonString = "{\"message\" : \"이건 클라이언트에서 서버로 보내는 패킷.\"}";
-            byte[] message = Encoding.UTF8.GetBytes(jsonString);
-            // 정수형 숫자를 버퍼에 넣을 때는 항상 바이트 오더를 조정해줘야함!
-            ushort length = (ushort)message.Length;
-            //length = (ushort)IPAddress.HostToNetworkOrder((short)length);
+            byte[] messageBuffer = Encoding.UTF8.GetBytes(message);
+            ushort length = (ushort)IPAddress.HostToNetworkOrder((short)messageBuffer.Length);
 
+            byte[] headerBuffer = BitConverter.GetBytes(length);
+
+            byte[] packetBuffer = new byte[headerBuffer.Length + messageBuffer.Length];
+            Buffer.BlockCopy(headerBuffer, 0, packetBuffer, 0, headerBuffer.Length);
+            Buffer.BlockCopy(messageBuffer, 0, packetBuffer, headerBuffer.Length, messageBuffer.Length);
+
+            int SendLength = toSocket.Send(packetBuffer, packetBuffer.Length, SocketFlags.None);
+        }
+
+        static void RecvPacket(Socket fromSocket, out string jsonString)
+        {
             byte[] lengthBuffer = new byte[2];
-            lengthBuffer = BitConverter.GetBytes(IPAddress.HostToNetworkOrder((short)length));
 
-            byte[] buffer = new byte[2 + length];
-
-            Buffer.BlockCopy(lengthBuffer, 0, buffer, 0, 2);
-            Buffer.BlockCopy(message, 0, buffer, 2, length);
-
-            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
-
-            IPEndPoint listenEndPoint = new IPEndPoint(IPAddress.Loopback, 4000);
-
-            clientSocket.Connect(listenEndPoint);
-
-            int SendLength = clientSocket.Send(buffer, buffer.Length, SocketFlags.None);
-
-            int RecvLength = clientSocket.Receive(lengthBuffer, 2, SocketFlags.None);
-            length = BitConverter.ToUInt16(lengthBuffer, 0);
+            int RecvLength = fromSocket.Receive(lengthBuffer, 2, SocketFlags.None);
+            ushort length = BitConverter.ToUInt16(lengthBuffer, 0);
             // 호스트 바이트 순서가 cpu, os에 따라 달라서 네트워크 바이트 순서로 바꿈
             length = (ushort)IPAddress.NetworkToHostOrder((short)length);
 
             byte[] recvBuffer = new byte[4096];
-            RecvLength = clientSocket.Receive(recvBuffer, length, SocketFlags.None);
+            RecvLength = fromSocket.Receive(recvBuffer, length, SocketFlags.None);
 
-            string JsonString = Encoding.UTF8.GetString(recvBuffer);
-            Console.WriteLine(JsonString);
+            jsonString = Encoding.UTF8.GetString(recvBuffer);
+        }
+
+        static void Main(string[] args)
+        {
+            Socket clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            //IPEndPoint listenEndPoint = new IPEndPoint(IPAddress.Parse("192.168.0.22"), 4000);
+            IPEndPoint listenEndPoint = new IPEndPoint(IPAddress.Parse("192.168.0.54"), 4000);
+
+            clientSocket.Connect(listenEndPoint);
+
+            //JObject result = new JObject();
+            //result.Add("code", "LogIn");
+            //result.Add("id", "minsuh");
+            //result.Add("password", "1234");
+            //SendPacket(clientSocket, result.ToString());
+
+            JObject result = new JObject();
+            result.Add("code", "SignUp");
+            result.Add("id", "cc");
+            result.Add("password", "1234");
+            result.Add("name", "cc");
+            result.Add("email", "cc@naver.com");
+            SendPacket(clientSocket, result.ToString());
+
+            string jsonString;
+            RecvPacket(clientSocket, out jsonString);
+
+            Console.WriteLine(jsonString);
 
             clientSocket.Close();
         }
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,55 +1,272 @@
-﻿
-using Newtonsoft.Json;
+﻿using MySqlConnector;
+using Newtonsoft.Json.Linq;
 using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Server
 {
     class Program
     {
+        static Socket listenSocket;
 
-        static void Main(string[] args)
+        // 공유 영역
+        static List<Socket> clientSockets = new List<Socket>();
+        //static List<Thread> threadManager = new List<Thread>();
+
+        static object _lock = new object();
+
+
+        static void AcceptThread()
         {
-            Socket listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            while (true)
+            {
+                Socket clientSocket = listenSocket.Accept();
 
-            IPEndPoint listenEndPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 4000);
+                lock (_lock)
+                {
+                    clientSockets.Add(clientSocket);
+                }
+                Console.WriteLine($"Connect client : {clientSocket.RemoteEndPoint}");
 
-            listenSocket.Bind(listenEndPoint);
-            listenSocket.Listen(10);
+                Thread workThread = new Thread(new ParameterizedThreadStart(WorkThread));
+                workThread.IsBackground = true;
+                workThread.Start(clientSocket);
+                //threadManager.Add(workThread);
+            }
+        }
 
-            Socket clientSocket = listenSocket.Accept();
+        static void WorkThread(Object clientObjectSocket)
+        {
+            // 아무거나 받을 수 있게 파라미터를 Object로 해놓고 쓰레드 함수 내에서 Socket으로 바꿔줌
+            Socket clientSocket = clientObjectSocket as Socket;
 
-            // 패킷 길이 받기(header)
-            byte[] headerBuffer = new byte[2];
-            int RecvLength = clientSocket.Receive(headerBuffer, 2, SocketFlags.None);
-            ushort packetLength = BitConverter.ToUInt16(headerBuffer, 0);
-            packetLength = (ushort)IPAddress.NetworkToHostOrder((short)packetLength);
+            while (true)
+            {
+                try
+                {
+                    byte[] headerBuffer = new byte[2];
+                    int RecvLength = clientSocket.Receive(headerBuffer, 2, SocketFlags.None);
+                    if (RecvLength > 0)
+                    {
+                        short packetlength = BitConverter.ToInt16(headerBuffer, 0);
+                        packetlength = IPAddress.NetworkToHostOrder(packetlength);
 
-            // 실제 패킷(header 길이 만큼)
-            byte[] dataBuffer = new byte[4096];
-            RecvLength = clientSocket.Receive(dataBuffer, packetLength, SocketFlags.None);
+                        byte[] dataBuffer = new byte[4096];
+                        RecvLength = clientSocket.Receive(dataBuffer, packetlength, SocketFlags.None);
+                        string JsonString = Encoding.UTF8.GetString(dataBuffer);
+                        Console.WriteLine(JsonString);
 
-            string JsonString = Encoding.UTF8.GetString(dataBuffer);
-            
-            Console.WriteLine(JsonString);
+                        string connectionString = "server=localhost;user=root;database=membership;password=sy103504";
+                        MySqlConnection mySqlConnection = new MySqlConnection(connectionString);
 
-            string message = "{\"message\" : \"서버가 잘 받음...\"}";
+                        JObject clientData = JObject.Parse(JsonString);
+
+                        string code = clientData.Value<string>("code");
+                        try
+                        {
+                            if (code.CompareTo("LogIn") == 0)
+                            {
+                                string userId = clientData.Value<string>("id");
+                                string userPassword = clientData.Value<string>("password");
+
+                                mySqlConnection.Open();
+                                MySqlCommand mySqlCommand = new MySqlCommand();
+
+                                mySqlCommand.Connection = mySqlConnection;
+                                // 로그인
+                                // user_id, user_password 에 $"{}"를 사용하면 보안에 좋지 않다!
+                                mySqlCommand.CommandText = "select * from users where user_id = @user_id and user_password = @user_password";
+                                mySqlCommand.Prepare();
+
+                                mySqlCommand.Parameters.AddWithValue("@user_id", userId);
+                                mySqlCommand.Parameters.AddWithValue("@user_password", userPassword);
+
+                                MySqlDataReader dataReader = mySqlCommand.ExecuteReader();
+                                if (dataReader.Read())
+                                {
+                                    // 로그인 성공
+                                    JObject result = new JObject();
+                                    result.Add("code", "loginresult");
+                                    result.Add("message", "success");
+
+                                    result.Add("name", dataReader["name"].ToString());
+                                    result.Add("email", dataReader["email"].ToString());
+                                    SendPacket(clientSocket, result.ToString());
+                                }
+                                else
+                                {
+                                    // 로그인 실패
+                                    JObject result = new JObject();
+                                    result.Add("code", "loginresult");
+                                    result.Add("message", "failed");
+
+                                    SendPacket(clientSocket, result.ToString());
+                                }
+                            }
+                            else if (code.CompareTo("SignUp") == 0)
+                            {
+                                string userId = clientData.Value<string>("id");
+                                string userPassword = clientData.Value<string>("password");
+                                string name = clientData.Value<string>("name");
+                                string email = clientData.Value<string>("email");
+
+                                mySqlConnection.Open();
+                                MySqlCommand mySqlCommand2 = new MySqlCommand();
+                                mySqlCommand2.Connection = mySqlConnection;
+                                mySqlCommand2.CommandText = "insert into users (user_id, user_password, name, email) values (@user_id, @user_password, @name, @email)";
+                                mySqlCommand2.Prepare();
+                                mySqlCommand2.Parameters.AddWithValue("@user_id",userId);
+                                mySqlCommand2.Parameters.AddWithValue("@user_password", userPassword);
+                                mySqlCommand2.Parameters.AddWithValue("@name", name);
+                                mySqlCommand2.Parameters.AddWithValue("@email", email);
+                                mySqlCommand2.ExecuteNonQuery();
+
+                                // 가입 성공
+                                JObject result = new JObject();
+                                result.Add("code", "signupresult");
+                                result.Add("message", "success");
+
+                                SendPacket(clientSocket, result.ToString());
+                            }
+                        }
+                        catch(Exception e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+                        finally
+                        {
+                            mySqlConnection.Close();
+                        }
+                    }
+                    else
+                    {
+                        //string message = "{ \"message\" : \" Disconnect : " + clientSocket.RemoteEndPoint + " \"}";
+                        //byte[] messageBuffer = Encoding.UTF8.GetBytes(message);
+                        //ushort length = (ushort)IPAddress.HostToNetworkOrder((short)messageBuffer.Length);
+
+                        //headerBuffer = BitConverter.GetBytes(length);
+
+                        //byte[] packetBuffer = new byte[headerBuffer.Length + messageBuffer.Length];
+                        //Buffer.BlockCopy(headerBuffer, 0, packetBuffer, 0, headerBuffer.Length);
+                        //Buffer.BlockCopy(messageBuffer, 0, packetBuffer, headerBuffer.Length, messageBuffer.Length);
+
+                        //clientSocket.Close();
+                        //lock (_lock)
+                        //{
+                        //    clientSockets.Remove(clientSocket);
+
+                        //    foreach (Socket sendSocket in clientSockets)
+                        //    {
+                        //        int SendLength = sendSocket.Send(packetBuffer, packetBuffer.Length, SocketFlags.None);
+                        //    }
+                        //}
+                        JObject result = new JObject();
+                        result.Add("code", "SendResult");
+                        result.Add("message", "failed");
+
+                        SendPacket(clientSocket, result.ToString());
+
+                        return;
+                    }
+                }
+                catch (Exception e)
+                {
+                    //Console.WriteLine($"Error 낸 놈 : {e.Message} {clientSocket.RemoteEndPoint}");
+
+                    //string message = "{ \"message\" : \" Disconnect : " + clientSocket.RemoteEndPoint + " \"}";
+                    //byte[] messageBuffer = Encoding.UTF8.GetBytes(message);
+                    //ushort length = (ushort)IPAddress.HostToNetworkOrder((short)messageBuffer.Length);
+
+                    //byte[] headerBuffer = new byte[2];
+
+                    //headerBuffer = BitConverter.GetBytes(length);
+
+                    //byte[] packetBuffer = new byte[headerBuffer.Length + messageBuffer.Length];
+                    //Buffer.BlockCopy(headerBuffer, 0, packetBuffer, 0, headerBuffer.Length);
+                    //Buffer.BlockCopy(messageBuffer, 0, packetBuffer, headerBuffer.Length, messageBuffer.Length);
+
+                    //clientSocket.Close();
+                    //lock (_lock)
+                    //{
+                    //    clientSockets.Remove(clientSocket);
+
+                    //    foreach (Socket sendSocket in clientSockets)
+                    //    {
+                    //        int SendLength = sendSocket.Send(packetBuffer, packetBuffer.Length, SocketFlags.None);
+                    //    }
+                    //}
+
+                    JObject result = new JObject();
+                    result.Add("code", "SendResult");
+                    result.Add("message", e.Message);
+
+                    SendPacket(clientSocket, result.ToString());
+
+                    return;
+                }
+            }
+        }
+
+        static void SendPacket(Socket toSocket, string message)
+        {
             byte[] messageBuffer = Encoding.UTF8.GetBytes(message);
             ushort length = (ushort)IPAddress.HostToNetworkOrder((short)messageBuffer.Length);
 
-            headerBuffer = BitConverter.GetBytes(length);
+            byte[] headerBuffer = BitConverter.GetBytes(length);
 
             byte[] packetBuffer = new byte[headerBuffer.Length + messageBuffer.Length];
-
             Buffer.BlockCopy(headerBuffer, 0, packetBuffer, 0, headerBuffer.Length);
-            Buffer.BlockCopy(messageBuffer, 0,packetBuffer, headerBuffer.Length, messageBuffer.Length);
+            Buffer.BlockCopy(messageBuffer, 0, packetBuffer, headerBuffer.Length, messageBuffer.Length);
 
-            int sendLength = clientSocket.Send(packetBuffer, packetBuffer.Length, SocketFlags.None);
+            int SendLength = toSocket.Send(packetBuffer, packetBuffer.Length, SocketFlags.None);
+        }
+        static void RecvPacket(Socket fromSocket, out string jsonString)
+        {
+            byte[] lengthBuffer = new byte[2];
 
-            clientSocket.Close();
+            int RecvLength = fromSocket.Receive(lengthBuffer, 2, SocketFlags.None);
+            ushort length = BitConverter.ToUInt16(lengthBuffer, 0);
+            // 호스트 바이트 순서가 cpu, os에 따라 달라서 네트워크 바이트 순서로 바꿈
+            length = (ushort)IPAddress.NetworkToHostOrder((short)length);
+
+            byte[] recvBuffer = new byte[4096];
+            RecvLength = fromSocket.Receive(recvBuffer, length, SocketFlags.None);
+
+            jsonString = Encoding.UTF8.GetString(recvBuffer);
+        }
+
+        static void Main(string[] args)
+        {
+            listenSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint listenEndPoint = new IPEndPoint(IPAddress.Parse("192.168.0.54"), 4000);
+
+            listenSocket.Bind(listenEndPoint);
+
+            listenSocket.Listen(10);
+
+            //Thread acceptThread = new Thread(new ThreadStart(AcceptThread));
+            //acceptThread.IsBackground = true;
+            //acceptThread.Start();
+            //acceptThread.Join();
+
+            // Task 버전
+            Task acceptTask = new Task(AcceptThread);
+            acceptTask.Start();
+            acceptTask.Wait();
+
+            // 만약 Task의 리턴값이 있다면
+            //if (acceptTask.IsCompleted)
+            //{
+            //    // 리턴값 받는 로직
+            //}
 
             listenSocket.Close();
         }
